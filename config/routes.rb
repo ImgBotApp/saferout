a2409edfd087dd88c576fe75c44f4b2c4f813fdb
zekebergida <zekebergida@gmail.com>
2017-10-16 07:29:38 -0400

    finish functionality features

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get '/collisions' => 'collisions#index'
+  get '/' => 'collisions#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
